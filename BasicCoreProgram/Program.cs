c927e695f82ef14f6bdd219b3b4f78193c0cb835
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-02-07 09:56:00 +0530

    [Ritesh]add :Swap Two Number

--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -10,7 +10,7 @@ namespace BasicCoreProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1 :FlipCoin \n 2 : Leap Year\n 3:Power Of Two\n 4:Harmonic Number\n 5:Calculatin Factor\n 6:Quotient Number ");
+            Console.WriteLine("1 :FlipCoin \n 2 : Leap Year\n 3:Power Of Two\n 4:Harmonic Number\n 5:Calculatin Factor\n 6:Quotient Number\n 7:Swap two Number ");
             Console.WriteLine("Enter a Option");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
@@ -38,6 +38,10 @@ namespace BasicCoreProgram
                 case 6:
                     QuotientRemainder remainder = new QuotientRemainder();
                     remainder.Division();
+                    break;
+                case 7:
+                    SwappingNumber Swaping = new SwappingNumber();
+                    Swaping.Swap();
                     break;
             }
             Console.ReadLine();
---
+++ BasicCoreProgram/SwappingNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class SwappingNumber
+    {
+        public void Swap()
+        {
+            Console.WriteLine("Enter The First Number :");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter The Second Number :");
+            int SecondNumber = Convert.ToInt32(Console.ReadLine());
+            firstNumber = firstNumber + SecondNumber;
+            SecondNumber = firstNumber - SecondNumber;
+            firstNumber = firstNumber - SecondNumber;
+            Console.WriteLine("After Swapping two Number : First Number Is=" + firstNumber + "and second number is " +SecondNumber);
+        }
+    }
+}
